2b3dd8dcd7f025cb04223b874838bafef665e3c1
MarcoSena2210 <msena2210@gmail.com>
2021-11-16 06:09:02 -0300

    first commit

---
+++ src/br/com/ms/exercicios/loops/Ex1_NomeIdade.java
@@ -0,0 +1,21 @@
+package br.com.ms.exercicios.loops;
+
+
+
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+
+public class Ex1_NomeIdade {
+
+	public static void main(String[] args) {
+		int a =5;
+		int b= 4;
+		System.out.print("Alo" + (a+b) );
+		
+
+	}
+
+}
